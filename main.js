06d5e85cfa528c8fb06eebd9a9cd4a41e114c64f
F3ffsz <89994033+F3ffsz@users.noreply.github.com>
2022-01-30 23:00:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var players = ['./Assets/Trivago.png', './Assets/Ayslan.png', './Assets/Predador.png', './Assets/Jeff.png']
+var result1, result2
+var player1 = document.getElementById('player1')
+var player2 = document.getElementById('player2')
+
+function sorteia() {
+    result1 = Math.round(Math.random() * 3)
+    result2 = Math.round(Math.random() * 3)
+    console.log(result1)
+ 
+    if (result1 == result2) {result2++}
+    console.log(result2)
+
+    player1.src = `${players[result1]}`
+    player2.src = `${players[result2]}`
+
+
+    console.log (`${players[result1]} e ${players[result2]}`)
+}
